b7918c2c158ce72623f7e94c1c78eecd6bc613ae
cyrildou <cyrildou@live.cn>
2024-04-13 22:58:37 +0800

    up

--- constant.go
+++ constant.go
@@ -13,6 +13,11 @@ const (
 )
 
 var FieldList = []string{
+	"short_name",    //TRXUSDT
+	"pro_name",      //BINANCE:TRXUSDT
+	"original_name", //BINANCE:TRXUSDT
+	"description",   //APPLE INC / US DOLLAR
+
 	"volume",  //当前volume
 	"lp",      //当前报价
 	"lp_time", //当前报价时间, 1712979679， 十位时间戳
@@ -24,30 +29,28 @@ var FieldList = []string{
 	"low_price",
 	"open_price",
 	"prev_close_price",
+
+	"type",                 //spot | forex  | index  |  stock  | bond | fund | futures
+	"logoid",               //indices/nasdaq-100  是指数的logo
 	"base_currency_id",     //base EUR  (EUR/USDT)
 	"base-currency-logoid", //base 图片地址(country/EU)
 	"currency_id",          //quote USD
 	"currency-logoid",      //country/US 这个是图片的地址
 	"currency_code",        //USD
+
 	"current_session",
-	"description", //具体pair的描述  TRON / TetherUS
-	"exchange",    //报价来源交易所  BINANCE
+	"exchange", //报价来源交易所  BINANCE
 	"format",
 	"fractional",  //false
 	"is_tradable", //true
 	"language",
 	"local_description",
 	"listed_exchange",
-	"logoid",
 	"minmov",
 	"minmove2",
-	"original_name", //BINANCE:TRXUSDT
-	"pricescale",    //100000
-	"pro_name",      //BINANCE:TRXUSDT
-	"short_name",    //TRXUSDT
-	"type",          //spot | forex
-	"typespecs",     //crypto | cfd
-	"update_mode",   //streaming
+	"pricescale",  //100000
+	"typespecs",   //crypto | cfd
+	"update_mode", //streaming
 	"variable_tick_size",
 	"value_unit_id",
 }
